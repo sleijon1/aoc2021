a6721a415ea6f53203cbe5bbdb0e4dd09d554a12
sleijon1 <simonleijon59@gmail.com>
2021-12-07 19:33:02 +0100

    d7 done

---
+++ d7/d7.py
@@ -0,0 +1,15 @@
+import os
+instructions = open(os.getcwd() + "\input.txt").read()
+positions = [int(pos) for pos in instructions.split(',')]
+
+def min_fuel(part_1=True):
+    fuels = []
+    for start_pos in positions:
+        fuel = 0
+        for pos in positions:
+            fuel += abs(start_pos-pos) if part_1 \
+                else sum(range(abs(start_pos-pos)+1))
+        fuels.append(fuel)
+    return min(fuels)
+
+print(f"Solution part 1: {min_fuel(True)}, part 2: {min_fuel(False)}")
