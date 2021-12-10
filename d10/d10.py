5bdcb18f1fccf2606026e5f14ed35f7104c9cc90
sleijon1 <simonleijon59@gmail.com>
2021-12-10 19:21:37 +0100

    removed redundant lists

--- d10/d10.py
+++ d10/d10.py
@@ -3,17 +3,15 @@ instructions = open(os.getcwd() + "\input.txt").read().splitlines()
 
 syn_map = {')': 3, ']': 57, '}': 1197, '>': 25137,}
 synp2_map = {')': 1, ']': 2, '}': 3, '>': 4}
-open = ['[', '{', '<', '(']
-close = [']', '}', '>', ')']
 matching = {'}': '{', ']': '[', '>': '<', ')':'('}
-matching_rev = {'{': '}', '[': ']', '<': '>', '(':')'}
+matching_rev = {v: k for k, v in matching.items()}
 
 def syntax_check(syn, i=0, opened=[]):
     try:
-        if syn[i] in open:
+        if syn[i] in matching.values():
             opened.insert(0, syn[i])
             return syntax_check(syn, i+1, opened)
-        elif syn[i] in close:
+        elif syn[i] in matching.keys():
             if syn[i-1] == matching[syn[i]]:
                 syn.pop(i-1)
                 syn.pop(i-1)
