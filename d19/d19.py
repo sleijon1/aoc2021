e4657aa0724c6453146b9aa05ce3ee3ca42e720c
sleijon1 <simonleijon59@gmail.com>
2021-12-20 21:39:56 +0100

    inefficient d19, improve later

--- d19/d19.py
+++ d19/d19.py
@@ -1,62 +1,75 @@
-from itertools import permutations
-def shift_positive(beac_dist: list):
-    x, y, z = beac_dist
-    min_x, min_y, min_z = min(x), min(y), min(z)
-    if min_x < 0:
-        beac_dist[0] = [val+abs(min_x) for val in x]
-    if min_y < 0:
-        beac_dist[1] = [val+abs(min_y) for val in y]
-    if min_z < 0:
-        beac_dist[0] = [val+abs(min_z) for val in z]
+import numpy as np
 
 def coord_sys_gen(beac_dist: list):
-    for face_dir in permutations([0, 1, 2]):
-        face_x, face_y, face_z = face_dir
-        print(face_dir)
-        up = [(1, 1, 1), (-1, 1, 1), (1, -1, 1), (1, 1, -1)]
-        for i, j, k in up:
-            print((i,j,k))
-            yield [[i*el for el in beac_dist[face_x]],
-            [j* el for el in beac_dist[face_y]],
-            [k*el for el in beac_dist[face_z]]]
+    directions = [0, 1, 2]
+    # inefficient, calculating all possible 48
+    for facing in directions:
+        up = [el for el in directions if el != facing]
+        up_axis = [((up[1], 1), (up[0], 1)), ((up[1], -1), (up[0], 1)), 
+                  ((up[0], -1), (up[1], 1)), ((up[0], -1), (up[1], -1)),
+                  ((up[0], 1), (up[1], 1)), ((up[1], -1), (up[0], -1)),
+                  ((up[0], 1), (up[1], -1)), ((up[1], 1), (up[0], -1))]
+        for fac_sign in (1, -1):
+            for (x, xs), (z, zs) in up_axis:
+                yield [xs*beac_dist[x],
+                       fac_sign*beac_dist[facing],
+                       zs*beac_dist[z]]
 
 if __name__=='__main__':
+    class LoopBreaker(Exception):
+        pass
+
     beac_dists = open("input.txt").read().split('\n\n')
     scanners = []
     for beac_dist in beac_dists:
-        x_list, y_list, z_list = [], [], []
-        scanners.append([x_list,y_list, z_list])
+        x_list, y_list, z_list = np.array([], dtype=np.int16), np.array([], 
+            dtype=np.int16), np.array([], dtype=np.int16)
         for dist in beac_dist.split('\n')[1:]:
             x, y, z = dist.split(',')
-            x_list.append(int(x))
-            y_list.append(int(y))
-            z_list.append(int(z))
-    #shift_positive(scanners[0])
-    curr_scanner = scanners[0]
-    print(curr_scanner)
-    gen = coord_sys_gen(curr_scanner)
-    for i, el in enumerate(gen):
-        print(el)
-    exit()
-    beacons = set()
-    scanners_add = 0
-    while True:
-        if scanners_add == len(scanners):
-            break
-        for scanner in scanners:
-            c_systems = coord_sys_gen(scanner)
-            beacon_cords = [(x, y) for x, y in zip(curr_scanner[0], curr_scanner[1])]
-            shared_beacons = 0
-            for c_system in c_systems:
-                shift_positive(c_system)
-                beacon_cords2 = [(x, y) for x, y in zip(c_system[0], c_system[1])]
-                for cord in beacon_cords2:
-                    if cord in beacon_cords:
-                        shared_beacons += 1
-                if shared_beacons >= 12:
-                    beacons.add(cord)
-                    curr_scanner = c_system
-                    scanners_add += 1
-                    break
-    print(f"Part 1: {len(beacons)}")
+            x_list = np.append(x_list, int(x))
+            y_list = np.append(y_list, int(y))
+            z_list = np.append(z_list, int(z))
+        scanners.append([x_list,y_list, z_list])
+    scanners_ps = []
+    abs_scanners = [curr_scanner:=scanners.pop(0)]
+    beacons = set((x, y, z) for x, y, z in zip(curr_scanner[0], curr_scanner[1], curr_scanner[2]))
+    while scanners:
+        try:
+            for scan_i, scanner in enumerate(scanners):
+                # curr scanner represents one of the "found" scanners
+                # scanner 0's coordinate system
+                for curr_scanner in abs_scanners:
+                    # c_systems represents all the possible coordinate system representations
+                    # of the next coordinate system we are trying to match
+                    c_systems = coord_sys_gen(scanner)
+                    beacon_cords = list(zip(curr_scanner[0], curr_scanner[1], curr_scanner[2]))
+                    for c_system in c_systems:
+                        shared_beacons = 0
+                        beacon_cords2 = list(zip(c_system[0], c_system[1], c_system[2]))
+                        for x, y, z in beacon_cords:
+                            for i, j, k in beacon_cords2:
+                                diff = (x-i, y-j, z-k)
+                                shared_beacons = 0
+                                for i, (a, b, c) in enumerate(beacon_cords2):
+                                    point_diff = (a+diff[0], b+diff[1], c+diff[2])
+                                    c_system[0][i], c_system[1][i], c_system[2][i] = point_diff
+                                    if point_diff in beacon_cords:
+                                        shared_beacons +=1
+                                if shared_beacons >= 12:
+                                    beacons.update(list(zip(c_system[0], c_system[1], c_system[2])))
+                                    abs_scanners.append(c_system)
+                                    # scanners absolute position in scanner[0]'s system
+                                    scanners_ps.append(diff)
+                                    scanners.pop(scan_i)
+                                    raise LoopBreaker
+        except LoopBreaker:
+            pass
+
+    max_dist = 0
+    for p1 in scanners_ps:
+        for p2 in scanners_ps:
+            mh = sum([abs(p1[0]-p2[0]), abs(p1[1]-p2[1]), abs(p1[2]-p2[2])])
+            if mh > max_dist:
+                max_dist = mh
+    print(f"Part 1: {len(beacons)}, Part 2: {max_dist}")
         
