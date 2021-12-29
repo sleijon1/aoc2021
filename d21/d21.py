2109fdf07b96f84cbfbefd60b5da456f80e21f42
sleijon1 <simonleijon59@gmail.com>
2021-12-29 21:59:24 +0100

    laptop latest

--- d21/d21.py
+++ d21/d21.py
@@ -1,27 +1,33 @@
 from copy import deepcopy
+from itertools import product 
 
 def quantum_memes(games, win_p1, win_p2):
-    splits = 0
+    sums = [sum(x) for x in product([1, 2, 3], [1, 2, 3], [1, 2, 3])]
+    # how many universes each roll appears in
+    roll_count = {x:sums.count(x) for x in sums}
+    first = True
     while games:
         p = games.pop()
-        while True:
-            for i in range(len(p)):
-                if p[i][0] >= 21:
-                    if p[i][2] == 1:
-                        win_p1[0] += 1
-                    else:
-                        win_p2[0] += 1
-                    break
-                for die_roll in range(1, 4):
-                    copy = deepcopy(p)
-                    copy[i][1] += die_roll
-                    copy[i][1] = copy[i][1] % 10 if copy[i][1] % 10 != 0 else 10
-                    copy[i][0] += copy[i][1]
-                    splits += 1
-                    games.append(copy)
-            if break_outer:
-                break
-    return splits
+        if p[0][0] >= 15:
+            if p[0][2] == 1:
+                win_p1[0] += p[0][3]
+            else:
+                win_p2[0] += p[0][3]
+            continue
+        elif not first:
+            p[0][3] *= (3**3)
+            p = p[::-1]
+        for die_roll in range(3, 10):
+            new_game = [[p[0][0], None, p[0][2], p[0][3], []], p[1]]
+            new_game[0][3] *= roll_count[die_roll]
+            new_game[0][4] = list(p[0][4])
+            new_game[0][4].append(die_roll)
+            new_pos = die_roll + p[0][1]
+            new_game[0][1] = new_pos % 10 if new_pos % 10 != 0 else 10
+            new_game[0][0] += new_game[0][1]
+            games.append(new_game)
+        first = False
+    return 
 
 if __name__ == '__main__':
     instr = open("input.txt").read().splitlines()
@@ -43,7 +49,7 @@ if __name__ == '__main__':
                 break
         if break_outer:
             break
-    splits = quantum_memes([[[0, p1_pos, 1], [0, p2_pos, 2]]], win_p1:=[0], win_p2:=[0])
+    quantum_memes([[[0, p1_pos, 1, 1, []], [0, p2_pos, 2, 1, []]]], win_p1:=[0], win_p2:=[0])
     print(win_p1[0], win_p2[0])
     print(f"Part 2: {max(win_p1[0], win_p2[0])}")
-    print(splits*max(win_p1[0], win_p2[0]))
+    print(max(win_p1[0], win_p2[0]))
